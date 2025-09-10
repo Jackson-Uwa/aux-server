dafa85e205ac24884c0076140a4daa7f0f4f113d
Jackson <jacksonuwa1@gmail.com>
2025-09-10 20:37:45 +0100

    Extra Features

--- backend/utils/errorHandler/error.js
+++ backend/utils/errorHandler/error.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
-  console.log(err);
+  // console.log(err);
 
   if (err.value !== req.params.id || err.name === "CastError") {
     const message = `Resource not found for ID: ${err.value}`;
